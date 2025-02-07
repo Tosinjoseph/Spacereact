04a77aed0fd78a367c3eb19027955ba6bafb7660
TosinJoseph <tosinjosephoffice2@gmail.com>
2025-02-07 14:23:26 -0800

    commit

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,17 +1,18 @@
 import './LoginSignup.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginSignup = ({loginShow,setLoginShow}) => {
+  const[loginChange, setLoginChange] =useState('Sign Up')
   return (
     <div className={`login-signup ${loginShow ? '' : "hide-show"}`}>
-        <h2>Login</h2>
-        <input type="text" placeholder='Enter your name' />
-        <input type="password" placeholder='Enter your password' />
-        <input type="email" placeholder='Enter your email' />
-        <p>Lost password? <span><a className="click" href='' >Click here</a></span></p>
+        <h2>{loginChange}</h2>
+        {loginChange==='Login'? <div></div> : <input type="text" placeholder='Enter your name' required /> }
+        <input type="password" placeholder='Enter your password' required/>
+        <input type="email" placeholder='Enter your email' required/>
+        {loginChange==="Sign Up"? <div></div> : <p>Lost password? <span><a className="click" href='' >Click here</a></span></p>}
         <div className="buttons">
-            <button type='submit'>Login</button>
-            <button type='submit'>Sign Up</button>
+            <button onClick={()=>setLoginChange('Login') } type='submit' className={loginChange==='Login' ? 'button login-grey' : 'button'}>Login</button>
+            <button onClick={()=>setLoginChange('Sign Up') } type='submit' className={loginChange==='Sign Up' ? 'button login-grey' : 'button'}>Sign Up</button>
         </div>
         <div onClick={()=> setLoginShow(false)} className='exit'>Exit</div>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,27 @@
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
 import {Link, ScrollLink} from 'react-scroll'
+import navi_icon from '../../assets/navi-icon.png'
 
-
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const[navToggle, setNavToggle] = useState(false);
+  const toggleNav =()=>{
+    navToggle? setNavToggle(false) : setNavToggle(true);
+  }
   return (
     <div className='navbar'>
         <div className="navlogo">
             <img src={logo} alt='' />
         </div>
-        <ul>
+        <ul className={navToggle? '' : 'nav-hide'}>
             <li><Link smooth={true} offset={20} duration={500} to='hero'>00 HOME</Link></li>
             <li><Link smooth={true} offset={-170} duration={500} to='destination'>01 DESTINATION</Link></li>
             <li><Link smooth={true} offset={-30} duration={500} to='crew'>02 CREW</Link></li>
             <li><Link smooth={true} offset={50} duration={500} to='technology'>03 TECHNOLOGY</Link></li>
         </ul>
+        <img src={navi_icon} alt=''  className='navi-icon' onClick={toggleNav}/>
     </div>
   )
 }
